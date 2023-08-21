1cc87df6bcd64b0bb22e5ff21f1294aba93b59dd
justfumz <esthergee01@gmail.com>
2023-08-21 20:02:07 +0100

    *hot fix*

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ console.log(
   `An event happened, on average, every ${90 / gameEvents.size} minutes`
 );
 time = [...gameEvents.keys()].pop();
-console.log(time);
+console.log(time);;
 console.log(
   `An event happened, on average, every ${time / gameEvents.size} minutes`
 );
